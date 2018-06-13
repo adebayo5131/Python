5d8e02f8e8a468b0ea147df31f2ff2f57cf44894
adebayo5131 <adebayoajagunna@yahoo.com>
2018-06-12 22:24:37 -0400

    test.py

---
+++ Test.py
@@ -0,0 +1,20 @@
+#def means to define a function
+
+"""
+comments on pyyton
+def means to define a function
+
+"""
+# package importing
+
+#Learning function in python
+
+def function(param1, param2):
+    var = param1+ param2
+    return var
+
+print(function(1,1))
+
+
+
+
