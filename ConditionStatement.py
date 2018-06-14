d49d10a374a02f145ed57a6e4b7ecb5d9e22b41e
adebayo5131 <adebayoajagunna@yahoo.com>
2018-06-14 02:05:52 -0400

    Add exisiting file

--- ConditionStatement.py
+++ ConditionStatement.py
@@ -10,12 +10,16 @@ elif(a == b):
 else:
     print("no")
 
+    print("\n")
+
 
     #while loop on python
 z =1
 while z <6:
     print(z)
     z += 1
+
+    print("\n")
 
 # #while loop with if statement
 # i = 1
@@ -28,4 +32,26 @@ while z <6:
 #For loop
 fruits = ["apple", "banana", "cherry"]
 for x in fruits:
+    if x=="banana":
+     break
+
+    print(x)
+
+    print("\n")
+
+#skip banan
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
+
+  print("\n")
+
+  # # Increment
+  # # the
+  # # sequence
+  # with 3(default is 1):
+
+  for x in range(2, 30, 3):
       print(x)
