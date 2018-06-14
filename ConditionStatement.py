8e5df840444407e11037e7e3575c52560872cadb
adebayo5131 <adebayoajagunna@yahoo.com>
2018-06-14 01:20:54 -0400

    Files

---
+++ ConditionStatement.py
@@ -0,0 +1,31 @@
+a = 33
+b = 33
+
+print(a+b)
+if a > b:
+    print("yes")
+elif(a == b):
+    print("They are equal")
+
+else:
+    print("no")
+
+
+    #while loop on python
+z =1
+while z <6:
+    print(z)
+    z += 1
+
+# #while loop with if statement
+# i = 1
+# while i < 6:
+#   print(i)
+#   if i == 3:
+#     break
+#   i += 1
+
+#For loop
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+      print(x)
--- Test.py
+++ Test.py
@@ -16,5 +16,51 @@ def function(param1, param2):
 print(function(1,1))
 
 
+print("Hello World")
 
 
+if 9 > 2:
+    print("yes")
+else:
+    print("no")
+
+a = "Adebayo"
+print("This is " + a)
+
+#Casting in Python
+
+x= int(1)
+y= int(2.7)
+z= int("3")
+
+print(x)
+print(y)
+print(z)
+
+#strip removes white spacing
+z ="    Yes!! "
+
+print(z.strip())
+
+#returns the length
+
+print(len("Adebayo"))
+
+
+#String input
+
+print("What is your name")
+
+r = input()
+
+print("Hello " + r)
+
+
+#List in python
+
+thisList =[1,2,3]
+print(thisList)
+
+#change 1-6
+thisList[1] = 6
+print(thisList)
