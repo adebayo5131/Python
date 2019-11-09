f5cd3c165bb78d297a27ad510ce5b874947930db
adebayo5131 <adebayoajagunna@gmail.com>
2019-11-09 02:32:44 -0500

    Updated StringSyntax

--- StringSyntax.py
+++ StringSyntax.py
@@ -11,4 +11,57 @@ print(word.islower())
 
 
 word2 = 'the begining of a new world'
-print(word2.title())
+print(word2.title()+'\n')
+
+#Join method
+joinMethod ='.'.join(['cats', 'dogs' ,'rats'])
+print(joinMethod)
+print()
+joinMethod =','.join(['cats', 'dogs' ,'rats'])
+print(joinMethod)
+print()
+joinMethod ='\n\n'.join(['cats', 'dogs' ,'rats'])
+print(joinMethod+'\n')
+
+splitMethod = 'This is how split method works'
+print(splitMethod.split())
+
+#split on a particular string
+splitMethod = 'This is how split method works'
+print(splitMethod.split('i'))
+
+#rjust() and ljust()
+
+string ='Hello'
+print(string.rjust(10) +'\n')
+
+string ='Hello'
+print(string.ljust(10) +'\n')
+
+string ='Hello'
+print(string.rjust(15,'*') +'\n')
+
+string ='Hello'
+
+print(string.ljust(15,'~') +'\n')
+
+string ='Hello'
+print(string.center(10,'*') +'\n')
+
+#strip removes white spaces
+
+word3 =" Adebayo Ajagunna"
+neword = word3
+print(word3.center(20,'*'))
+print(neword.strip())
+
+#lstrip() rstrip()
+
+print('x     '.strip())
+
+print('Adebayo Edward AjagunnA'.strip('Ae'))
+
+#Repalce
+
+wordToReplace ='Adebayo Ajagunna'
+print('\n'+wordToReplace.replace('a', 'o'))
