172db5429c64088ccc4415f3dc5d599686fe1847
adebayo5131 <adebayoajagunna@gmail.com>
2019-12-01 00:47:51 -0500

    Recursion and Iteration

---
+++ Recursion and Iteration.py
@@ -0,0 +1,13 @@
+def iteration(low,high):
+    while low <= high:
+        print(low)
+        low = low+1
+        
+def Recursive(low,high):
+    if low <= hight:
+        print(low)
+        Recursive(low+1, high)
+
+
+print(iteration(1,5))
+print(Recursive(1,5))
