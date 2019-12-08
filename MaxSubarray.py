a698b041b8b58e6624bc98f6c6344cf888cb0047
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2019-12-08 05:08:24 -0500

    MaxSubarray LeetCode

--- MaxSubarray.py
+++ MaxSubarray.py
@@ -1,5 +1,5 @@
 print('''
-            Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
 
 Example:
 
