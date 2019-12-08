983abc3c968f2a6eeec02eea134f2e6c9750beaf
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2019-12-07 23:02:35 -0500

    Group Anagrams Leetcode

---
+++ groupAnagramsLeetCode.py
@@ -0,0 +1,20 @@
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        
+        """
+        dic = {}
+        for items in strs:
+            sortedItems = ''.join(sorted(items))
+            if sortedItems in dic:
+                dic.get(sortedItems).append(items)
+            else:
+                dic[sortedItems] = [items]
+        return dic.values()
+
+answer = Solution()
+n  = ["eat", "tea","tap" ,"tan", "ate", "nat"]
+print(answer.groupAnagrams(n))
