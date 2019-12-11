bd40e311bfaf8835e4307456113f99b9184e42d1
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2019-12-11 12:31:01 -0500

    FindMidPointCourse

---
+++ FindMipPointCourse.py
@@ -0,0 +1,32 @@
+
+def findMidPointCourse(pairs):
+
+    memo = {}
+    checker ={}
+    mid = len(pairs)//2
+    pl = len(pairs)
+    start =""
+
+    for i in range(pl):
+        memo[pairs[i][1]] = pairs[i][0]
+        checker[pairs[i][0]] = pairs[i][1]
+        if pairs[i][0] not in memo:
+            start = pairs[i][0]
+
+    for i in range(mid):
+        start = checker[start]
+
+    return start
+    
+
+
+
+pairs = [['DataStructures','Algorithm'],
+         ['FoundationofCS',' OperatingSystem'],
+         ['ComputerNetworking','ComputerArchitecture'],
+         ['Algorithms','FoundationofCS'],
+         ['ComputerArchitecture','DatStructures'],
+         ['SoftwareDesign','ComputerNetworking']
+         ]
+
+print(findMidPointCourse(pairs))
