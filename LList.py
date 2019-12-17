a503310b33a4c84aa4bb3206c7aeabcbdd638c69
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2019-12-17 01:09:46 -0500

    added reverse linkedList

--- LList.py
+++ LList.py
@@ -1,34 +1,71 @@
-
 class Node:
-    def __init__(self, val):
-        self.val = val
+    def __init__ (self, data=None):
+        self.data = data
         self.next = None
-
+        
 class llist:
-    def __init__(self):
+    def __init__ (self):
         self.head = None
-
-    def append(self,new_val):
-
-        new_node = Node(new_val)
-
+        self.size = 0
+        
+    
+    def append (self, data = None):
+        node = Node(data)
+        
         if self.head is None:
-            self.head = new_node
-
+            self.head = node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = node
+    
+    def iter(self):
+        
         current = self.head
-        while self.head:
-           current.next = new_node
+        while current:
+            self.size+=1
+            print(current.data)
+            current = current.next
+    
+    def removeNode(self, data):
+        current = self.head
+        prev = self.head
+        while current:
+            if current.data == data:
+                if current == self.head:
+                    self.head = current.next
+                else:
+                    prev.next = current.next
+                    self.size-=1
+            prev = current
+            current = current.next
+            
+        
+            
+    def reverseList(self):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        current = self.head
+        previous = None
+        while  current:
+            nxt =  current.next
+            current.next = previous
+            previous =  current
+            current = nxt
+        self.head = previous       
+     
+     
+        
+            
+        
+link = llist()
+    
+link.append(5)
+link.append(6)
+link.append(4)
 
-    def print(self):
-
-        while self.head:
-            print(self.head.next)
-
-
-ll = llist()
-
-ll.append(5)
-ll.append(6)
-ll.append(4)
-ll.print()
-
+link.reverseList()
+link.iter()
