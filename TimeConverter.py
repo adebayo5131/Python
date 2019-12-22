076c57d260ee41a9dc9894eafb0b22ba619892b0
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2019-12-22 14:47:32 -0500

    Updated Time Converter GG

--- TimeConverter.py
+++ TimeConverter.py
@@ -11,39 +11,42 @@ def solution(T):
     elif t1 == "??" and t2 == "??":
         return "23:59"
     else:
-        s = list(t1)
-        if t1[0] == "?":
-            if int(t1[1]) > 3:
-                s[0] = "1"
-                t1 = "".join(s)
-            else:
-                s[0] = "2"
-                t1 = "".join(s)
+        if t1 == "??":
+            s = "23"
+            t1 = "".join(s)
+        else:
+            s = list(t1)
+            if t1[0] == "?":
+                if int(t1[1]) > 3:
+                    s[0] = "1"
+                    t1 = "".join(s)
+                else:
+                    s[0] = "2"
+                    t1 = "".join(s)
 
-        # list one after split
-        if t1[1] == "?":
-            if int(t1[0]) == 1:
-                s[1] = "9"
-                t1 = "".join(s)
-            else:
-                s[1] = "3"
-                t2 = "".join(s)
-
-    s = list(t2)
-    if t2[0] and t2[1] == "?":
-        s[0] = "5"
-        s[1] = "9"
+            # list one after split
+            if t1[1] == "?":
+                if int(t1[0]) == 1:
+                    s[1] = "9"
+                    t1 = "".join(s)
+                else:
+                    s[1] = "3"
+                    t1 = "".join(s)
+    if t2 == "":
+        s = "59"
         t2 = "".join(s)
-    elif t2[0] == "?":
+    else:
         s = list(t2)
-        s[0] = "5"
-        t2 = "".join(s)
-    elif t2[1] == "?":
-        s = list(t2)
-        s[1] = "9"
-        t2 = "".join(s)
+        if t2[0] == "?":
+            s = list(t2)
+            s[0] = "5"
+            t2 = "".join(s)
+        elif t2[1] == "?":
+            s = list(t2)
+            s[1] = "9"
+            t2 = "".join(s)
 
     return t1 + ":" + t2
 
 
-print(solution("1?:??"))
+print(solution("12:11"))
