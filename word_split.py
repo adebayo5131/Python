cdc72179b40d8012926be590f0f3c253ea87b4a9
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2019-12-22 16:49:43 -0500

    Word split

--- word_split.py
+++ word_split.py
@@ -1,18 +1,18 @@
-def word_split(s,wordDict):
+def word_split(s, wordDict):
     n = len(s)
     solutions = {}
     solutions[n] = ['']
 
     def solve(i):
         if i not in solutions:
-            solutions[i] = [s[i:j]+ (tail and(' ' + tail))
-                            for j in range(i,n+1)
+            solutions[i] = [s[i:j] + (tail and(' ' + tail))
+                            for j in range(i, n+1)
                             if s[i:j] in wordDict
-
                             for tail in solve(j)]
         return solutions[i]
     solve(0)
     return solutions[0]
+
 
 wordDict = set(["face", "book", "facebook", "bo", "ok"])
 print(word_split("facebook", wordDict))
