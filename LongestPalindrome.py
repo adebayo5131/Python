ae3eb7ce7bd3a0548939d06ac885540664355bda
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2019-12-21 19:08:51 -0500

    LongestPalindrome Substring

--- LongestPalindrome.py
+++ LongestPalindrome.py
@@ -1,1 +1,21 @@
+import re
+def longestPalindromicSubstring(string):
+	checker = ""
+	for i in range(len(string)):
+		for j in range(i, len(string)):
+			subString = string[i:j+1]
+			if len(subString) > len(checker) and isPalindrome(subString):
+				checker = subString
+	return checker
+				
+def isPalindrome(s):
+	leftIndex = 0
+	rightIndex = len(s)-1
+	while leftIndex < rightIndex:
+		if s[leftIndex] != s[rightIndex]:
+			return False
+		leftIndex+=1
+		rightIndex-=1
+	return True
 
+print(longestPalindromicSubstring("z234a5abbba54a32z"))
