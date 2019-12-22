8dc91d0d38adf9b8a7a5e25d1dee50b0ac3a753e
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2019-12-22 15:01:15 -0500

    Get Minimum Difference

--- MaximumDifference.py
+++ MaximumDifference.py
@@ -1,33 +1,19 @@
 def MaxDiff(arr):
-    # if max(arr) == arr[0]:
-    #     arr.remove(arr[0])
-    # if arr == []:
-    #     return 0
-    # if len(arr) == 1:
-    #     return 0
 
-    # j = 0
-    # for i in range(1, len(arr)):
-    #     maxNum = arr[j]
-    #     if arr[i] > maxNum:
-    #         maxNum = arr[i]
-    #         j += 1
-
-    # arr = arr[:i]
-
-    # minNum = min(arr)
-
-    # return maxNum - minNum
+    if arr == []:
+        return 0
+    if len(arr) == 1:
+        return 0
     if max(arr) == arr[0]:
         arr.remove(arr[0])
 
     minNum = arr[0]
     maxNum = arr[0]
-    for i in range(1, len(arr)):
+    for i in range(len(arr)):
         minNum = min(minNum, arr[i])
         maxNum = max(maxNum, arr[i])
     return (maxNum - minNum)
 
 
-n = [100,40,90]
+n = [100, 40, 90, 10]
 print(MaxDiff(n))
