fc673e5ff2cdfd4761f474cec5ec4aead04677b6
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2019-12-24 13:32:45 -0500

    RelativeSort

--- RelativeSort.py
+++ RelativeSort.py
@@ -1,27 +1,25 @@
 
 def relativeSortArray(arr1, arr2):
- 
-    memo = {}
+
+    memo = dict()
     returnValue = list()
-    
-    for i in  range(len(arr1)):
-        if arr1[i]  in memo:
-            memo[arr1[i]] +=1
+
+    for i in arr1:
+        if i in memo:
+            memo[i] += 1
         else:
-            memo[arr1[i]] = 1  
-    
+            memo[i] = 1
+
     for i in arr2:
         if i in memo:
-            returnValue.extend([i]*memo.pop(i)) 
-    
+            returnValue.extend([i] * memo.pop(i))
+
     for i in memo.keys():
         returnValue.extend([i]*memo[i])
     return returnValue
-        
-    
-    
 
-arr1 = [2,3,1,3,2,4,6,7,9,2,19]
-arr2 = [2,1,4,3,9,6]
-print(relativeSortArray(arr1,arr2))
-#[2,2,2,1,4,3,3,9,6,7,19]
+
+arr1 = [2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19]
+arr2 = [2, 1, 4, 3, 9, 6]
+print(relativeSortArray(arr1, arr2))
+# [2,2,2,1,4,3,3,9,6,7,19]
