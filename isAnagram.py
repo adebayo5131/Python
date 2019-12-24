bc476b85e9979ab4a0069e05bf5c30e0cfe91eb5
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2019-12-24 03:45:12 -0500

    Is anagram improved

--- isAnagram.py
+++ isAnagram.py
@@ -5,37 +5,33 @@ def isAnagram(s, t):
     :rtype: bool
     """
     dic = {}
-    re = True
+    re = bool
 
     if len(s) != len(t):
-        re = False
-    else:
-        if s == t:
-            re = True
+        return False
+
+    if s == t:
+        return True
+
+    for i in s:
+        if i in dic:
+            dic[i] += 1
         else:
-            for i in s:
-                if i in dic:
-                    dic[i] += 1
-                else:
-                    dic[i] = 1
+            dic[i] = 1
 
-            for j in t:
-                if j not in dic:
-                    re = False
-                    break
-                else:
-                    dic[j] -= 1
+    for j in t:
+        if j not in dic:
+            return False
+        else:
+            dic[j] -= 1
 
-            for k in dic:
-                if dic[k] != 0:
-                    re = False
-                    break
-                else:
-                    re = True
-
-    return re
+    for k in dic:
+        if dic[k] != 0:
+            return False
+        else:
+            return True
 
 
 s = "anagram"
-t = "nagaram"
+t = "nagarak"
 print(isAnagram(s, t))
