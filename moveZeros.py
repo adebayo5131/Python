f0ba6e6c8e7891378a8edb5aa20652edbdba61af
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2019-12-24 22:49:07 -0500

    Move zeros

--- moveZeros.py
+++ moveZeros.py
@@ -15,6 +15,13 @@ def moveZeroes(nums):
             i += 1
     return nums
 
+    #OR this
+#         j = 0
+#         for i in range(len(nums)):
+#             if nums[i] != 0:
+#                 nums[i], nums[j] = nums[j], nums[i]
+#                 j += 1
+
 
 n = [0, 1, 0, 3, 12]
 print(moveZeroes(n))
