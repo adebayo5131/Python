961aeffc9c133d9d2af73ed01b974003db551333
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2019-12-25 10:23:21 -0500

    Max Subarray

--- MaxSubarray.py
+++ MaxSubarray.py
@@ -1,4 +1,4 @@
-print('''
+'''
 Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
 
 Example:
@@ -6,14 +6,19 @@ Example:
 Input: [-2,1,-3,4,-1,2,1,-5,4],
 Output: 6
 Explanation: [4,-1,2,1] has the largest sum = 6.
-''')
+'''
 
 
-def maxSubArray(self, nums):
+class Solution(object):
+    def maxSubArray(nums):
 
-    ult_sum =nums[0]
-    for i in range(1,len(nums)): 
-        nums[i]= max(nums[i], nums[i]+nums[i-1]) 
-        ult_sum= max(ult_sum,nums[i]) 
-    return ult_sum  
-    
+        largestSum = nums[0]
+
+        for i in range(1, len(nums)):
+            nums[i] = max(nums[i], nums[i]+nums[i-1])
+            largestSum = max(largestSum, nums[i])
+        return largestSum
+
+
+array = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+print(Solution.maxSubArray(array))
