f868358ba78f680e75054a3b16af6cec197fee7b
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2020-01-03 06:27:55 -0500

    Kadane's Algorithm

---
+++ Kadanes.py
@@ -0,0 +1,15 @@
+# O(n) time and O(1) space
+def kadanesAlgorithm(array):
+    # Write your code here.
+	maxEnding = array[0]
+	maxSoFar = array[0]
+
+	for i in range(1, len(array)):
+		current = array[i]
+		new_Sum = maxEnding + current
+		maxEnding = max(current, new_Sum)
+		maxSoFar = max(maxSoFar, maxEnding)
+	return maxSoFar
+
+
+print(kadanesAlgorithm([3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4]))
