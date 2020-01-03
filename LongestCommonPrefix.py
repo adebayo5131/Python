edb36779690712e0d2cc2b429ae9694e8634cec1
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2020-01-03 14:48:18 -0500

    Update LongestCommon Prefix

--- LongestCommonPrefix.py
+++ LongestCommonPrefix.py
@@ -1,18 +1,20 @@
 class Solution:
     def longestCommonPrefix(self, strs):
 
-        if not strs: 
+        if not strs:
             return ""
         if len(strs) == 1:
             return strs[0]
+
         strs.sort()
         p = ""
         for i, j in zip(strs[0], strs[-1]):
             if i == j:
-                p+=i
+                p += i
             else:
                 break
         return p
+
 
 check = Solution()
 str = ["Flowers", "Floor", "Flora"]
