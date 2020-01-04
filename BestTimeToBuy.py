dae0e73a74ec7a47d7ab683349fe89d0c5ded5fd
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2020-01-04 14:35:17 -0500

    Best Time to Buy

--- BestTimeToBuy.py
+++ BestTimeToBuy.py
@@ -1,19 +1,18 @@
 
 # O(n) and O(1) space
 
-
-def maxProfit(prices):
-    maxprofit = 0
+def bestBuy(prices):
+    maxProfit = 0
     if prices is not None:
-        minbuy = prices[0]
-        for i in range(len(prices) - 1):
-            if prices[i+1] - minbuy < 0:
-                minbuy = prices[i+1]
+        minBuy = prices[0]
+        for price in range(len(prices) - 1):
+            checkProfit = prices[price + 1] - minBuy
+            if checkProfit < 0:
+                minBuy = prices[price+1]
             else:
-                if prices[i+1]-minbuy > maxprofit:
-                    maxprofit = prices[i+1] - minbuy
-    return maxprofit
-
-
-n = [7, 1, 5, 3, 6, 4]
-print(maxProfit(n))
+                if checkProfit > maxProfit:
+                    maxProfit = checkProfit
+        
+    return maxProfit
+                
+print(bestBuy([7, 1, 5, 3, 6, 4]))
