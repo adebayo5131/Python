30cc8e62f83b27f8d03a9b7c7e7a25e4f53264f0
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2020-01-05 01:54:23 -0500

    Updated plusOne

---
+++ DeleteDuplicateSorted.py
@@ -0,0 +1,15 @@
+def deleteDup(A):
+    j = 1
+    for i in range(1, len(A)):
+        if A[j-1] != A[i]:
+            A[j] = A[i]
+            j += 1
+
+    for i in range(j, len(A)):
+        A[i] = 0
+
+    return A
+
+
+arrayA = [1, 1, 2, 2, 3, 3, 4]
+print(deleteDup(arrayA))
--- PlusOneLeet.py
+++ PlusOneLeet.py
@@ -3,16 +3,27 @@ def plusOne(digits):
     :type digits: List[int]
     :rtype: List[int]
     """
-
-    d = len(digits)-1
-    for i in range(d, -1, -1):
-        if digits[i] != 9:
-            digits[i] += 1
-            return digits
+    digits[-1] += 1
+    for i in range(len(digits)-1, 0, -1):
+        if digits[i] != 10:
+            break
         digits[i] = 0
-        if i == 0:
-            digits.insert(0, 1)
-            return digits
+        digits[i - 1] += 1
+
+    if digits[0] == 10:
+        digits[0] = 1
+        digits.append(0)
+    return digits
+
+    # d = len(digits)-1
+    # for i in range(d, -1, -1):
+    #     if digits[i] != 9:
+    #         digits[i] += 1
+    #         return digits
+    #     digits[i] = 0
+    #     if i == 0:
+    #         digits.insert(0, 1)
+    #         return digits
 
 
-print(plusOne([9]))
+print(plusOne([9, 9, 9]))
