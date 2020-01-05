93bb9afbc54cfe6a69f448630eff07cc3be85f90
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2020-01-05 14:28:41 -0500

    Prime numbers

---
+++ PrimeNumbers.py
@@ -0,0 +1,14 @@
+def generatePrimes(n):
+    primes = []
+
+    isPrime = [False, False] + [True] * (n - 1)
+
+    for i in range(2, n + 1):
+        if isPrime[i]:
+            primes.append(i)
+            for j in range(i, n + 1, i):
+                isPrime[j] = False
+    return primes
+
+
+print(generatePrimes(18))
