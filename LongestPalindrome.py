2b4bac80e05ced2cee07df60287476d19a24e95a
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2020-01-06 22:09:36 -0500

    LongestPalindrome Substring

--- LongestPalindrome.py
+++ LongestPalindrome.py
@@ -1,22 +1,32 @@
 def longestPalindromicSubstring(string):
     checker = ""
-    string = string.replace(' ', '')
     for i in range(len(string)):
         for j in range(i, len(string)):
             subString = string[i:j+1]
             if len(subString) > len(checker) and isPalindrome(subString):
                 checker = subString
     return checker
-
-
+    
 def isPalindrome(s):
-    w = ""
-    for i in s:
-        w = i + w
-    if w == s:
-        return True
-    else:
-        return False
+    left = 0
+    right = len(s)-1
+    
+    while left < right:
+        if s[left] != s[right]:
+            return False
+        left+=1
+        right-=1
+    return True
+            
+
+# def isPalindrome(s):
+#     w = ""
+#     for i in s:
+#         w = i + w
+#         if w == s:
+#             return True
+
+#     return False
 
 
 print(longestPalindromicSubstring("it's Honeynoon"))
