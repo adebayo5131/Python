63cbd1b4dba48572847de778b0785328fae55ee7
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2020-01-11 12:06:56 -0500

    Counting Palindrom substring

---
+++ countinSubstring.py
@@ -0,0 +1,29 @@
+def countSubstrings(s):
+    count = 0
+    for i in range(len(s)):
+        for j in range(i, len(s)):
+            substring = s[i:j+1]
+            if checkPalindrome(substring):
+                count += 1
+    return count
+
+
+def checkPalindrome(s):
+    check = True
+    w = ''
+    for i in s:
+        w = i + w
+        if w == s:
+            return check
+    return False
+#     left = 0
+#     right = len(s)-1
+
+
+#     while left <= right:
+#         if s[left] != s[right]:
+#             return False
+#         left+=1
+#         right-=1
+#     return True
+print(countSubstrings("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"))
