ce28de21f30453163a81ec65e09509ce245c3011
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2020-01-14 00:59:00 -0500

    Update sequential Digits

--- sequentialDigits.py
+++ sequentialDigits.py
@@ -3,15 +3,17 @@ from collections import deque
 
 def sortedList(low, high):
     array = []
-    q = deque(range(1, 9))
-    while q:
-        num = q.popleft()
+    # q = deque(range(1, 9))
+    queue = [1,2,3,4,5,6,7,8,9]
+    while queue:
+        # num = queue.popleft()
+        num = queue.pop(0)
         if num > high:
             break
         if low <= num:
             array.append(num)
         if num % 10 + 1 < 10:
-            q.append(num * 10 + (num % 10 + 1))
+            queue.append(num * 10 + (num % 10 + 1))
     return array
 
 
