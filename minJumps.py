6feb4281cdfdb3e645cdcbc9c958dc6c81712587
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2020-01-15 11:10:47 -0500

    Min Jumps

---
+++ minJumps.py
@@ -0,0 +1,17 @@
+def minNumberOfJumps(array):
+    if len(array) == 1:
+        return 0
+
+    jumps = 0
+    maxReach = steps = array[0]
+
+    for i in range(1, len(array)-1):
+        maxReach = max(maxReach, i + array[i])
+        steps -= 1
+        if steps == 0:
+            jumps += 1
+            steps = maxReach - i
+    return jumps + 1
+
+
+print(minNumberOfJumps([3, 4, 2, 1, 2, 3, 7, 1, 1, 1, 3]))
