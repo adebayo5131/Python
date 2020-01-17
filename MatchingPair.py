bb9f1a15698521e2af2ef1881ce6e55ff99725b8
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2020-01-17 10:45:40 -0500

    Matching Pair

--- MatchingPair.py
+++ MatchingPair.py
@@ -1,13 +1,26 @@
 #O(n^2) | O(n) space
 def findmatching(arr, t):
-    sums = []
-    for i in range(len(arr)):
-        for j in range(i+1, len(arr)):
-            if arr[i]+ arr[j] == t:
-               sums.append(sorted([arr[i], arr[j]]))
-    return sums
-            
+    # sums = []
+    # for i in range(len(arr)):
+    #     for j in range(i+1, len(arr)):
+    #         if arr[i]+ arr[j] == t:
+    #            sums.append(sorted([arr[i], arr[j]]))
+    # return sums
+    arr.sort()
 
+    left = 0
+    right = len(arr) - 1
+    pairs = []
+    while left < right:
+        sums = arr[left] + arr[right]
+        if sums == t:
+            pairs.append([arr[left], arr[right]])
+        if sums > t:
+            right -= 1
+        else:
+            left += 1
+    return pairs
+        
 
 t = 16
 arr = [11, 15,1, 6, 8, 9, 10]
