8d16fef0877ea7e603fcd63ce224ca1c02c924e5
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2020-01-20 22:18:26 -0500

    nestedArray

---
+++ nestedArray.py
@@ -0,0 +1,21 @@
+def arrayNesting(nums):
+    if not nums:
+        return 0
+
+    seen = set()
+
+    longest = float('-inf')
+    for i in range(len(nums)):
+        if nums[i] not in seen:
+            countlongest = 0
+            while nums[i] not in seen:
+                seen.add(nums[i])
+                i = nums[i]
+                countlongest += 1
+            longest = max(longest, countlongest)
+    return longest
+
+
+A = [5, 4, 0, 3, 1, 6, 2]
+print(arrayNesting(A))
+n
