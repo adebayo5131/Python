4db70311cdf9be606c5e2452d102dc1c84a40ed6
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2020-01-23 15:54:20 -0500

    Reverse A string

--- ReverseAstring.py
+++ ReverseAstring.py
@@ -1,22 +1,24 @@
 def Reverse(n):
     s = ''
-    
+
     for i in n:
-       s = i + s
+        s = i + s
     return s
-print(Reverse('adebayo'))
 
 
-def reverse(n):
-    output = [""]*len(n)
-    i = len(n) -1
-    
-    if n == " ":
-        return (" ")
-    else:
-        
-        for j in n:
-            output[i] = j
-            i-=1
-        return ''.join(output)
-print(reverse(" "))
+print(Reverse('ram is costly'))
+
+
+# def reverse(n):
+#     output = [""]*len(n)
+#     i = len(n) -1
+
+#     if n == " ":
+#         return (" ")
+#     else:
+
+#         for j in n:
+#             output[i] = j
+#             i-=1
+#         return ''.join(output)
+# print(reverse(" "))
