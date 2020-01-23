e6eb71701a1f262f2589366c2a687494c8f4d56b
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2020-01-23 15:15:48 -0500

    Sorting 3 unique element using 0(n) time

---
+++ dutch_flag_partition.py
@@ -0,0 +1,79 @@
+def dutch_flag_partition(array):
+
+    # secondLargest = findSecondLargest(array)
+
+    secondLargest = betterFindSecond(array)
+    for i in array:
+        if i == secondLargest:
+            pivot_index = i
+
+    return sortNums(array, pivot_index)
+
+
+def sortNums(array, pivot_index):
+    pivot = array[pivot_index]
+    smaller = 0
+
+    for i in range(len(array)):
+        if array[i] < pivot:
+            array[i], array[smaller] = array[smaller], array[i]
+            smaller += 1
+
+    larger = len(array) - 1
+
+    for i in reversed(range(len(array))):
+        if array[i] < pivot:
+            break
+        elif array[i] > pivot:
+            array[i], array[larger] = array[larger], array[i]
+            larger -= 1
+    return array
+
+
+
+##Without better second largest
+def betterFindSecond(array):
+    minNumber = min(array)
+    maxNumber = max(array)
+
+    for i in array:
+        if i != maxNumber and i != minNumber:
+            return array.index(i)
+
+# def findSecondLargest(array):
+    
+#     largest = [None, None, None]
+
+#     for i in range(len(array)):
+#         updateLargest(array[i], largest)
+#         if largest[0] and largest[1] and largest[2]:
+#             break
+            
+#     for i in reversed(range(len(largest)-1)):
+#         if largest[i] > largest[i+1]:
+#             largest[i],largest[i+1] = largest[i+1], largest[i]
+#     return array.index(largest[1])
+
+
+# def updateLargest(num, largest):
+    
+#     if not largest[0] or not largest[1] or not largest[2]:
+#         if not largest[2]:
+#             shiftUpdate(num, largest, 2)
+#         elif not largest[1] and largest[2] != num:
+#             shiftUpdate(num, largest, 1)
+#         elif not largest[0] and (largest[1] != num and largest[2] != num):
+#             shiftUpdate(num, largest, 0)  
+
+    
+
+# def shiftUpdate(num, largest, position):
+
+#     for i in range(position + 1):
+
+#         if i == position:
+#             largest[i] = num
+#         else:
+#             largest[i] = largest[i+1]
+
+print(dutch_flag_partition([3, 3, 2, 1, 3, 2, 1]))
