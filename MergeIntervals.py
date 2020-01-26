2d0b4cb8c017fda78ae0e5d0d0d25c88dd67b4f7
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2020-01-26 12:02:30 -0500

    Merge Intervals

---
+++ MergeIntervals.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        if not intervals:
+            return []
+        intervals.sort()
+        newIntervals = [intervals[0]]
+
+        for i in range(1, len(intervals)):
+            if newIntervals[-1][-1] >= intervals[i][0]:
+                maxEnding = max(newIntervals[-1][-1], intervals[i][1])
+                newIntervals[-1][-1] = maxEnding
+            else:
+                newIntervals.append(intervals[i])
+        return newIntervals
+
+
+test = Solution()
+
+print(test.merge([[1, 3], [2, 6], [8, 10], [15, 18]]))
