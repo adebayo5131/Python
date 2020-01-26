ee33b2d1bb2d7ad8791b4370c6641240d4cd234e
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2020-01-26 12:00:31 -0500

    Combination Sums

--- combinationSum.py
+++ combinationSum.py
@@ -26,29 +26,5 @@ def recurse(candidates, target, start, val, newSet):
                 candidates[i], i, val, newSet + [candidates[i]])
 
 
-# Better Solution
-
-# def combinationSum(self, candidates, target):
-#     """
-#     :type candidates: List[int]
-#     :type target: int
-#     :rtype: List[List[int]]
-#     """
-#     res = []
-#     candidates = sorted(candidates)
-#     start = 0
-#     ans = []
-#     self.dfs(start, ans, target, res, candidates)
-#     return res
-
-# def dfs(start, ans, target, res, candidates):
-#     if target == 0:
-#         res.append(ans)
-#         return
-
-#     for i in range(start, len(candidates)):
-#         if target < candidates[i]:
-#             break
-#         self.dfs(i, ans + [candidates[i]], target - candidates[i], res, candidates)
 
 print(print(combinationSum([2, 3, 5], 8)))
